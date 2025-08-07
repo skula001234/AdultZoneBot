d0e84fc2aecae1d12c53e3fe69bff17799cd67da
We R Developer's <werdeveloper.official@gmail.com>
2025-08-07 10:15:33 +0530

    Added main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from bot import bot
+from pyrogram import idle
+
+async def start():
+    await bot.start()
+
+bot.loop.create_task(start())
+idle()
+
+'''
+🛡️ Developed by: WeRDeveloper
+🚫 Do not attempt to sell, copy, or redistribute this code.
+
+🔗 Official Telegram Channel: https://t.me/WeRdevelopers
+📩 Contact (Verified Only): https://t.me/WeRDevX
+▶️ YouTube Channel: https://www.youtube.com/@WeRdevelopers
+
+⚠️ Only @WeRDevX is officially managed. Any other accounts are fake.
+'''
