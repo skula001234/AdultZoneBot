7ce102a7a69760a167abef7b580cb98cad05a9fd
Sakula <mohittatu2007@gmail.com>
2025-08-13 16:02:30 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,17 +1,17 @@
 import os
 from typing import List
 
-API_ID = int(os.getenv("API_ID", ""))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7624888045:AAFIS3GtRHXy")
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://xxxxx:mkooaa@werdeveloper.vxfam.mongodb.net/")
+API_ID = int(os.getenv("API_ID", "22422690"))
+API_HASH = os.getenv("API_HASH", "2884fbc53b44a1bbcfa7c525e185c1aa")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8409559700:AAE4mvKnJAOgKqT2pmTW1hCD2ZViBq9Ta08")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://dhruvkachhiya75622:c4gb6msfE0jzGtFa@cluster0.6e8wrs6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_CHANNEL_ID = int(os.getenv("DATABASE_CHANNEL_ID", "-10026550xxxx"))
-ADMIN_ID = int(os.getenv("ADMIN_ID", "xxxxxxxx"))
-LOG_CHNL = int(os.getenv("LOG_CHNL", "-1002357280xxx"))
-ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "") # Without @
+ADMIN_ID = int(os.getenv("ADMIN_ID", "7793257011"))
+LOG_CHNL = int(os.getenv("LOG_CHNL", "-1002724729272"))
+ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "mrfrontman001") # Without @
 IS_FSUB = bool(os.environ.get("FSUB", True))
-AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-1002528152xxx").split()))
-DATABASE_CHANNEL_LOG = int(os.getenv("DATABASE_CHANNEL_ID", "-1002652997xxx"))
+AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-1002713153687").split()))
+DATABASE_CHANNEL_LOG = int(os.getenv("DATABASE_CHANNEL_ID", "-1002713153687"))
 FREE_VIDEO_DURATION = int(os.getenv("FREE_VIDEO_DURATION", "240"))
 
 '''
@@ -24,3 +24,4 @@ FREE_VIDEO_DURATION = int(os.getenv("FREE_VIDEO_DURATION", "240"))
 
 ⚠️ Only @WeRDevX is officially managed. Any other accounts are fake.
 '''
+
